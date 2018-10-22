45ac661d22dc0c8a7365752855951ee22b4a8bff
stdmatt <stdmatt@pixelwizards.io>
2018-10-22 18:04:49 +0100

    - Start implementing stuff for youtube-dl.

---
+++ src/youtube-dl.sh
@@ -0,0 +1,28 @@
+##~---------------------------------------------------------------------------##
+##                        _      _                 _   _                      ##
+##                    ___| |_ __| |_ __ ___   __ _| |_| |_                    ##
+##                   / __| __/ _` | '_ ` _ \ / _` | __| __|                   ##
+##                   \__ \ || (_| | | | | | | (_| | |_| |_                    ##
+##                   |___/\__\__,_|_| |_| |_|\__,_|\__|\__|                   ##
+##                                                                            ##
+##  File      : youtube.sh                                                    ##
+##  Project   : dots                                                          ##
+##  Date      : Oct 22, 2018                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : stdmatt <stdmatt@pixelwizards.io>                             ##
+##  Copyright : stdmatt - 2018                                                ##
+##                                                                            ##
+##  Description :                                                             ##
+##    Stuff to make the youtube-dl easier to use.                             ##
+##---------------------------------------------------------------------------~##
+
+
+youtube-dl-mp3()
+{
+    local URL="$1";
+    test -z "$URL"                        \
+        && echo "Empty url - Aborting..." \
+        return 1;
+
+    youtube-dl --extract-audio --audio-format mp3 "$URL";
+}
