19ac01ee2c68ee093e07781299206939722f3e2a
stdmatt <stdmatt@pixelwizards.io>
2019-02-07 09:45:44 +0000

    - Add things for 8ballpool mobile.

--- src/miniclip.sh
+++ src/miniclip.sh
@@ -19,14 +19,56 @@
 ##----------------------------------------------------------------------------##
 ## Aliaes                                                                     ##
 ##----------------------------------------------------------------------------##
-##------------------------------------------------------------------------------
-## 8BPE
-alias mpt_8bpe_build="BUILD_ENV=Staging sh build.sh emscripten && npm run debug";
-alias mpt_8bpe_clean="BUILD_ENV=Staging sh build.sh clean";
-alias mpt_8bpe_run="npm run server"
+# ##------------------------------------------------------------------------------
+# ## 8BPE
+# alias mpt_8bpe_build="BUILD_ENV=Staging sh build.sh emscripten && npm run debug";
+# alias mpt_8bpe_clean="BUILD_ENV=Staging sh build.sh clean";
+# alias mpt_8bpe_run="npm run server"
 
 
+# ##------------------------------------------------------------------------------
+# ## PACIO
+# alias mpt_pacio_start_maestro="gosh maestro && make seixas_console";
+# alias mpt_pacio_start_core="gosh core && npm run serve";
+
+
+##----------------------------------------------------------------------------##
+## 8 Ball Pool Mobile                                                         ##
+##----------------------------------------------------------------------------##
 ##------------------------------------------------------------------------------
-## PACIO
-alias mpt_pacio_start_maestro="gosh maestro && make seixas_console";
-alias mpt_pacio_start_core="gosh core && npm run serve";
+_mpt_8bpm_build()
+{
+    ## improve(stdmatt): Go the target directory???
+    local platform="$1";
+    local mode="$2";
+    local install="$3";
+
+    sh build.sh "$platform" "$mode" "$install";
+}
+
+##------------------------------------------------------------------------------
+mpt_8bpm_build_android()
+{
+    local mode="debug";
+    test -n "$1" && mode="$1";
+
+    _mpt_8bpm_build "android" "$mode" "install";
+}
+
+##------------------------------------------------------------------------------
+mpt_8bpm_build_mac()
+{
+    local mode="debug";
+    test -n "$1" && mode="$1";
+
+    _mpt_8bpm_build "mac" "$mode";
+}
+
+##------------------------------------------------------------------------------
+mpt_8bpm_build_emscripten()
+{
+    local mode="debug";
+    test -n "$1" && mode="$1";
+
+    _mpt_8bpm_build "emscripten" "$mode";
+}
