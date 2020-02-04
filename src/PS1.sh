8ba0904ba8e3191807160cb7b96fc73f2ce95e62
stdmatt <stdmatt@pixelwizards.io>
2020-02-04 16:35:27 +0300

    Add random prompts...

--- src/PS1.sh
+++ src/PS1.sh
@@ -10,7 +10,7 @@
 ##  Date      : Jan 06, 2019                                                  ##
 ##  License   : GPLv3                                                         ##
 ##  Author    : stdmatt <stdmatt@pixelwizards.io>                             ##
-##  Copyright : stdmatt - 2019                                                ##
+##  Copyright : stdmatt - 2019, 2020                                          ##
 ##                                                                            ##
 ##  Description :                                                             ##
 ##    My custom PS1.                                                          ##
@@ -20,20 +20,23 @@
 ##----------------------------------------------------------------------------##
 ## Constants                                                                  ##
 ##----------------------------------------------------------------------------##
-COLOR_PWD=64
-COLOR_GIT=241
-COLOR_PROMPT=247
+_DOTS_COLOR_PWD=64
+_DOTS_COLOR_GIT=241
+_DOTS_COLOR_PROMPT=247
 
-STR_PROMPT="λ";
+##
+## Prompt
+_DOTS_PROMPT_LAMBDA="λ";
+_DOTS_PROMPT_SMILE=":)";
 
 
 ##----------------------------------------------------------------------------##
 ## Functions                                                                  ##
 ##----------------------------------------------------------------------------##
 ##------------------------------------------------------------------------------
-parse_git_branch()
+_dots_parse_git_branch()
 {
-    BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+    local BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
     if [ ! "${BRANCH}" == "" ]; then
         local TAG=$(git describe --tags $(git rev-list --tags --max-count=1) 2> /dev/null);
         if [ -n "$TAG" ]; then
@@ -46,9 +49,24 @@ parse_git_branch()
     fi
 }
 
+##------------------------------------------------------------------------------
+_dots_random_prompt()
+{
+    ## @todo(stdmatt): Put the prompts on an array and 
+    ## make it choose them automatically...
+    local COUNT=2;
+    local INDEX=$(( RANDOM % COUNT ));
+    if [ $INDEX == 0 ]; then 
+        echo "$_DOTS_PROMPT_LAMBDA";
+    else 
+        echo "$_DOTS_PROMPT_SMILE";
+    fi;
+}
 
 ##----------------------------------------------------------------------------##
 ## Exports                                                                    ##
 ##----------------------------------------------------------------------------##
+_DOTS_STR_PROMPT=$(_dots_random_prompt)
+
 ##------------------------------------------------------------------------------
-export PS1="\n\[\033[38;5;${COLOR_PWD}m\]\W\[$(tput sgr0)\]:\[\033[38;5;${COLOR_GIT}m\]\`parse_git_branch\`\[$(tput sgr0)\]\n\[$(tput bold)\033[38;5;${COLOR_PROMPT}m\]${STR_PROMPT}\[$(tput sgr0)\] "
+export PS1="\n\[\033[38;5;${_DOTS_COLOR_PWD}m\]\W\[$(tput sgr0)\]:\[\033[38;5;${_DOTS_COLOR_GIT}m\]\`_dots_parse_git_branch\`\[$(tput sgr0)\]\n\[$(tput bold)\033[38;5;${_DOTS_COLOR_PROMPT}m\]${_DOTS_STR_PROMPT}\[$(tput sgr0)\] "
