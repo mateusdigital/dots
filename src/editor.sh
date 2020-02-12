7af00a782b1ab31f1cfcb23f2d63946b34ffe469
stdmatt <stdmatt@pixelwizards.io>
2020-02-12 08:51:35 +0300

    Export the EDITOR and VISUAL vars.

--- src/editor.sh
+++ src/editor.sh
@@ -1,9 +1,14 @@
+##------------------------------------------------------------------------------
 STDMATT_EDITORS="codium   \
                  vscodium \
                  code     \
                  vim      \
                  nano";
 
+export EDITOR=vim;
+export VISUAL=vim;
+
+##------------------------------------------------------------------------------
 function ed()
 {
     for EDITOR in $STDMATT_EDITORS; do
