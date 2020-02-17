0788403e76d9d949f8a84330892fb96304df341d
stdmatt <stdmatt@pixelwizards.io>
2020-02-17 12:44:14 +0300

    Rework the PS1 prompt so now we can change colors easily.

--- src/PS1.sh
+++ src/PS1.sh
@@ -16,6 +16,11 @@
 ##    My custom PS1.                                                          ##
 ##---------------------------------------------------------------------------~##
 
+##----------------------------------------------------------------------------##
+## Imports                                                                    ##
+##----------------------------------------------------------------------------##
+source /usr/local/src/stdmatt/shellscript_utils/main.sh
+
 
 ##----------------------------------------------------------------------------##
 ## Constants                                                                  ##
@@ -33,40 +38,65 @@ _DOTS_PROMPT_SMILE=":)";
 ##----------------------------------------------------------------------------##
 ## Functions                                                                  ##
 ##----------------------------------------------------------------------------##
+_dots_color()
+{
+    local COLOR=$1;
+    local MSG=$2;
+
+    echo -e "\033[38;5;${COLOR}m"$MSG"\033[0m";
+}
+
 ##------------------------------------------------------------------------------
 _dots_parse_git_branch()
 {
     local BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+    local STR="";
     if [ ! "${BRANCH}" == "" ]; then
         local TAG=$(git describe --tags $(git rev-list --tags --max-count=1) 2> /dev/null);
         if [ -n "$TAG" ]; then
-            echo "(${BRANCH}:${TAG})";
+            STR="(${BRANCH}:${TAG})";
         else
-            echo "(${BRANCH})";
+            STR="(${BRANCH})";
         fi;
     else
-       echo ""
+       STR="";
     fi
+    echo $(_dots_color 245 $STR);
+}
+
+_dots_get_pwd()
+{
+    echo $(_dots_color 202 $(basename $PWD));
 }
 
 ##------------------------------------------------------------------------------
 _dots_random_prompt()
 {
-    ## @todo(stdmatt): Put the prompts on an array and 
+    ## @todo(stdmatt): Put the prompts on an array and
     ## make it choose them automatically...
     local COUNT=2;
     local INDEX=$(( RANDOM % COUNT ));
-    if [ $INDEX == 0 ]; then 
-        echo "$_DOTS_PROMPT_LAMBDA";
-    else 
-        echo "$_DOTS_PROMPT_SMILE";
+    local PROMPT="";
+    if [ $INDEX == 0 ]; then
+        PROMPT="$_DOTS_PROMPT_LAMBDA";
+    else
+        PROMPT="$_DOTS_PROMPT_SMILE";
     fi;
+
+    echo "$(_dots_color 252 $PROMPT)";
+}
+
+##------------------------------------------------------------------------------
+_dots_get_os_name()
+{
+    echo "$(_dots_color 75 $(pw_os_get_simple_name))";
 }
 
 ##----------------------------------------------------------------------------##
 ## Exports                                                                    ##
 ##----------------------------------------------------------------------------##
-_DOTS_STR_PROMPT=$(_dots_random_prompt)
+_DOTS_STR_PROMPT="$(_dots_random_prompt)";
+_DOTS_OS_NAME="$(_dots_get_os_name)"
 
 ##------------------------------------------------------------------------------
-export PS1="\n\[\033[38;5;${_DOTS_COLOR_PWD}m\]\W\[$(tput sgr0)\]:\[\033[38;5;${_DOTS_COLOR_GIT}m\]\`_dots_parse_git_branch\`\[$(tput sgr0)\]\n\[$(tput bold)\033[38;5;${_DOTS_COLOR_PROMPT}m\]${_DOTS_STR_PROMPT}\[$(tput sgr0)\] "
+export PS1="\n${_DOTS_OS_NAME}:`_dots_get_pwd`:`_dots_parse_git_branch`\n${_DOTS_STR_PROMPT} "
