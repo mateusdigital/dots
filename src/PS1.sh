c14b673ba06f0dadddcff7f4a5bcdb45899844c6
stdmatt <stdmatt@pixelwizards.io>
2020-02-18 14:53:46 +0300

    Add support for 8-bit color terminals

--- src/PS1.sh
+++ src/PS1.sh
@@ -30,6 +30,11 @@ _DOTS_COLOR_PWD=64
 _DOTS_COLOR_GIT=245
 _DOTS_COLOR_PROMPT=252
 
+_DOTS_COLOR_OS_8=33
+_DOTS_COLOR_PWD_8=32
+_DOTS_COLOR_GIT_8=36
+_DOTS_COLOR_PROMPT_8=37
+
 ##
 ## Prompt
 _DOTS_PROMPT_LAMBDA="λ";
@@ -39,12 +44,35 @@ _DOTS_PROMPT_SMILE=":)";
 ##----------------------------------------------------------------------------##
 ## Functions                                                                  ##
 ##----------------------------------------------------------------------------##
-_dots_color()
+_dots_color_256()
 {
     local COLOR=$1;
     local MSG=$2;
 
     echo -e "\033[38;5;${COLOR}m"$MSG"\033[0m";
+}
+
+_dots_color_8()
+{
+    local COLOR=$1;
+    local MSG=$2;
+
+    echo -e "\033[0${COLOR}m"$MSG"\033[0m";
+}
+
+if [ $(tput colors) == 8 ]; then
+    __dots_color=_dots_color_8;
+    _DOTS_COLOR_OS=$_DOTS_COLOR_OS_8
+    _DOTS_COLOR_PWD=$_DOTS_COLOR_PWD_8
+    _DOTS_COLOR_GIT=$_DOTS_COLOR_GIT_8
+    _DOTS_COLOR_PROMPT=$_DOTS_COLOR_PROMPT_8
+else
+    __dots_color=_dots_color_256;
+fi;
+
+_dots_color()
+{
+    $__dots_color $@
 }
 
 ##------------------------------------------------------------------------------
@@ -66,6 +94,7 @@ _dots_parse_git_branch()
     echo $(_dots_color $_DOTS_COLOR_GIT $STR);
 }
 
+##------------------------------------------------------------------------------
 _dots_get_pwd()
 {
     echo $(_dots_color $_DOTS_COLOR_PWD $(basename $(pwd)));
@@ -93,6 +122,8 @@ _dots_get_os_name()
 {
     echo "$(_dots_color $_DOTS_COLOR_OS $(pw_os_get_simple_name))";
 }
+
+
 
 ##----------------------------------------------------------------------------##
 ## Exports                                                                    ##
