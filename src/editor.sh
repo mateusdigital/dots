9a302ff1b07fac0a6ed6ad06953e899020d35d7d
stdmatt <stdmatt@pixelwizards.io>
2020-02-26 12:20:18 +0300

    Remove old stuff

--- src/editor.sh
+++ src/editor.sh
@@ -15,28 +15,9 @@
 ##  Description :                                                             ##
 ##                                                                            ##
 ##---------------------------------------------------------------------------~##
-##------------------------------------------------------------------------------
-STDMATT_EDITORS="codium   \
-                 vscodium \
-                 code     \
-                 vim      \
-                 nano";
 
 ##----------------------------------------------------------------------------##
 ## Exports                                                                    ##
 ##----------------------------------------------------------------------------##
 export EDITOR=vim;
 export VISUAL=vim;
-
-##------------------------------------------------------------------------------
-function ed()
-{
-    for EDITOR in $STDMATT_EDITORS; do
-        ## echo "Trying editor: $EDITOR";
-        local EDITOR_PATH="$(pw_get_program_path $EDITOR)";
-        if [ -n "$EDITOR_PATH" ]; then
-            $EDITOR_PATH $@;
-            return;
-        fi;
-    done;
-}
