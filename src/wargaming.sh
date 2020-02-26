d6318afa42cccf35540df49d866abd1745df43bc
stdmatt <stdmatt@pixelwizards.io>
2020-02-26 12:20:12 +0300

    Add the license headers

--- src/wargaming.sh
+++ src/wargaming.sh
@@ -1,3 +1,21 @@
+##~---------------------------------------------------------------------------##
+##                        _      _                 _   _                      ##
+##                    ___| |_ __| |_ __ ___   __ _| |_| |_                    ##
+##                   / __| __/ _` | '_ ` _ \ / _` | __| __|                   ##
+##                   \__ \ || (_| | | | | | | (_| | |_| |_                    ##
+##                   |___/\__\__,_|_| |_| |_|\__,_|\__|\__|                   ##
+##                                                                            ##
+##  File      : wargaming.sh                                                  ##
+##  Project   : dots                                                          ##
+##  Date      : Feb 03, 2020                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : stdmatt <stdmatt@pixelwizards.io>                             ##
+##  Copyright : stdmatt 2020                                                  ##
+##                                                                            ##
+##  Description :                                                             ##
+##                                                                            ##
+##---------------------------------------------------------------------------~##
+
 connect-clickhouse-client()
 {
     docker run -it --rm --link clickhouse_storage_database:clickhouse_storage_database yandex/clickhouse-client --host clickhouse_storage_database
