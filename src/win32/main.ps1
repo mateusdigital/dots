2020dc66a81f1c69bfe0b53149f900849a2ace66
stdmatt <stdmatt@pixelwizards.io>
2020-12-22 14:28:26 +0200

    Add new functions to make Windows bareable...

--- src/win32/main.ps1
+++ src/win32/main.ps1
@@ -1,3 +1,5 @@
+$POWERSHELL_TELEMETRY_OPTOUT = 0;
+$DOTS_PATH = "D:/stdmatt/dots"; ## @todo(stdmatt): Make the installation set this var.... Dec 22, 2020
 
 ##
 ## Files
@@ -20,7 +22,7 @@ function files()
 
 
 ##
-## Paths
+## Ubisoft
 ##
 ##------------------------------------------------------------------------------
 function lty
@@ -29,12 +31,17 @@ function lty
     pwd
 }
 
+##------------------------------------------------------------------------------
 function clean-game-profile()
 {
     rm C:\Users\mmesquita\Documents\ProjectLiberty -Recurse
 }
 
-
+##------------------------------------------------------------------------------
+function edit-game-ini()
+{
+    code --new-window E:/mmesquita_KIV-WKS-PR536_9193/bin/scimitar.ini
+}
 
 ##------------------------------------------------------------------------------
 function ack
@@ -43,11 +50,26 @@ function ack
     pwd
 }
 
+
+##
+## Paths
+##
+##------------------------------------------------------------------------------
+$STDMATT_BIN_FOLDER_NAME = ".stdmatt_bin";
+$STDMATT_BIN_PATH        = Join-Path -Path $env:USERPROFILE -ChildPath $STDMATT_BIN_FOLDER_NAME;
+
 ##------------------------------------------------------------------------------
 function me
 {
     cd  D:/stdmatt
     pwd
+}
+
+##------------------------------------------------------------------------------
+function me-bin()
+{
+    cd $STDMATT_BIN_PATH;
+    pwd;
 }
 
 
@@ -66,12 +88,51 @@ function reload-profile()
     & $profile
 }
 
+## Sync...
+##------------------------------------------------------------------------------
+$TERMINAL_SETTINGS_INSTALL_FULLPATH = "C:\Users\mmesquita\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json";
+$TERMINAL_SETTINGS_SOURCE_FULLPATH = "$DOTS_PATH" + "/extras/windows_terminal.json";
+
+$PROFILE_INSTALL_FULLPATH = "C:/Users/mmesquita/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1"
+$PROFILE_SOURCE_FULLPATH = "$DOTS_PATH" + "/src/win32/main.ps1";
+
+##------------------------------------------------------------------------------
+function _copy_newer_file()
+{
+    $filename_1 = $args[0];
+    $filename_2 = $args[1];
+    $time_1 = (Get-Item $filename_1).LastWriteTime;
+    $time_2 = (Get-Item $filename_2).LastWriteTime;
+
+    $src_filename = $filename_1;
+    $dst_filename = $filename_2;
+
+    if ($time_1 -lt $time_2) {
+        $src_filename = $filename_2;
+        $dst_filename = $filename_1;
+    } else {
+        $src_filename = $filename_1;
+        $dst_filename = $filename_2;
+    }
+
+    echo "[sync-profile] Copying ($src_filename) to ($dst_filename)";
+    Copy-Item $src_filename $dst_filename -Force;
+}
+
+##------------------------------------------------------------------------------
+function sync-profile()
+{
+    _copy_newer_file $TERMINAL_SETTINGS_INSTALL_FULLPATH $TERMINAL_SETTINGS_SOURCE_FULLPATH
+    _copy_newer_file $PROFILE_INSTALL_FULLPATH           $PROFILE_SOURCE_FULLPATH
+}
+
 
 ##
 ## Utils
 ##
 ##------------------------------------------------------------------------------
-$JOURNAL_PATH = "C:/Users/mmesquita/Desktop/Journal";
+$JOURNAL_PATH       = "C:/Users/mmesquita/Desktop/Journal";
+$GAMES_TO_MAKE_PATH = "D:/stdmatt/games_to_make";
 
 ##------------------------------------------------------------------------------
 function journal()
@@ -89,6 +150,11 @@ function journal()
     code $JOURNAL_PATH;
 }
 
+##------------------------------------------------------------------------------
+function to-make()
+{
+    code $GAMES_TO_MAKE_PATH;
+}
 
 ##
 ## Shell
@@ -99,3 +165,64 @@ function global:prompt
     $curr_path = pwd;
     return "$curr_path `n:) "
 }
+
+
+##
+## youtube-dl
+##
+##------------------------------------------------------------------------------
+$YOUTUBE_DL_EXE_PATH = Join-Path -Path $STDMATT_BIN_PATH -ChildPath "youtube-dl.exe";
+
+function _ensure_youtube_dl()
+{
+    if( -not(Test-Path -Path $YOUTUBE_DL_EXE_PATH) ) {
+        echo "[youtube-dl-mp3] Could not find youtube-dl at ($YOUTUBE_DL_EXE_PATH)- Aborting...";
+        return 0;
+    }
+    return 1;
+}
+
+function youtube-dl-mp3()
+{
+    if(-not(_ensure_youtube_dl)) {
+        return;
+    }
+
+
+    $URL = $args[0];
+    if([string]::IsNullOrWhiteSpace($URL)) {
+        echo "[youtube-dl-mp3] Empty url - Aborting...";
+        return;
+    }
+
+    & $YOUTUBE_DL_EXE_PATH --no-playlist --extract-audio --audio-format mp3 $URL;
+}
+
+
+# ##------------------------------------------------------------------------------
+# function youtube-dl-playlist()
+# {
+#     local URL="$1";
+#     test -z "$URL"                                              \
+#         && echo "[youtube-dl-playlist] Empty url - Aborting..." \
+#         return 1;
+
+#     youtube-dl -o                                             \
+#         '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' \
+#         "$URL";
+# }
+
+# ##------------------------------------------------------------------------------
+# function youtube-dl-music-playlist()
+# {
+#     local URL="$1";
+#     test -z "$URL"                                              \
+#         && echo "[youtube-dl-playlist] Empty url - Aborting..." \
+#         return 1;
+
+#     youtube-dl                                                \
+#         --extract-audio --audio-format mp3                    \
+#         -o                                                    \
+#         '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' \
+#         "$URL";
+# }
