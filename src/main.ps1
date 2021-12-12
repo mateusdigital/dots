d74e7cda346d5e975b7321165a7a76034705af12
stdmatt <stdmatt@pixelwizards.io>
2021-12-12 13:35:18 -0800

    - [EXTERNAL] Chocolately..

--- src/main.ps1
+++ src/main.ps1
@@ -949,4 +949,8 @@ function http-server()
 cls
 Get-Date
 
-install-binaries;
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
