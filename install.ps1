7ef231f8fa259c7342f8d33372c1ea92e9c27036
stdmatt <stdmatt@pixelwizards.io>
2022-02-08 08:35:00 -0800

    - [BUGFIX] Call operator.

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@
 ## Script
 $SCRIPT_FULLPATH = $MyInvocation.MyCommand.Path;
 $SCRIPT_DIR      = (Split-Path "$SCRIPT_FULLPATH" -Parent);
-$DOTS_FULLPATH   = $(Join-Path $SCRIPT_DIR "src" "main.ps1");
+$DOTS_FULLPATH   = (Join-Path $SCRIPT_DIR "src" "main.ps1");
 
 ##
 ## Install Profile
