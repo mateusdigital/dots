b3a44b35dac6492add0645f3b03cb72083abc7fa
stdmatt <stdmatt@pixelwizards.io>
2022-02-27 13:48:16 -0300

    [WIP] Improve the code and features.
    - Add possibility of measuring multiple times.
    - Output the sorted (worse->best) sorted values.
    - Output the total time that took to run the test.

--- tests/measure_total.ps1
+++ tests/measure_total.ps1
@@ -1,46 +1,50 @@
 . $profile
 
-$SCRIPT_DIR = (sh_dirpath $MyInvocation.InvocationName)
+$SCRIPT_DIR          = (sh_dirpath $MyInvocation.InvocationName)
 $MEASURE_OUTPUT_FILE = (sh_join_path $SCRIPT_DIR "result.txt");
 
-## Measure it and just save:
-##   Line Number - Time Taken - Line
-Measure-Script $PROFILE
-    | tail +7                 `
-    | grep -v "00:00.0000000" `
-    | grep  "00:00."          `
-    | tr -s " "               `
-    | cut -d " " -f 3-1000    `
-    > $MEASURE_OUTPUT_FILE
+$measures   = @{};
+$acc_time   = 0.0;
+$runs_count = 1;
+$script_to_measure = $profile;
 
+for($i = 0; $i -lt $runs_count; $i += 1) {
+    ## Measure it and just save:
+    ##   Line Number - Time Taken - Line
+    Measure-Script $PROFILE       `
+        | tail +7                 `
+        | grep -v "00:00.0000000" `
+        | grep  "00:00."          `
+        | tr -s " "               `
+        | cut -d " " -f 3-1000    `
+        > $MEASURE_OUTPUT_FILE
 
-$entries       = @();
-$total_time    = 0.0;
-$slowest_entry = $null;
+    $file = (Get-Content "$MEASURE_OUTPUT_FILE");
+    foreach ($line in $file) {
+        $comps = $line.Split(" ");
 
-$file = (Get-Content "$MEASURE_OUTPUT_FILE")
-foreach ($line in $file) {
-    $comps = $line.Split(" ");
+        $line_no   = [int]  ($comps[0]);
+        $exec_time = [float]($comps[1].Split(":")[1]);
+        $statement = $comps[2..$comps.Length];
 
-    $line_no   = $comps[0];
-    $exec_time = $comps[1].Split(":")[1];
-    $statement = $comps[2..$comps.Length]
-    $obj = [pscustomobject]@{
-        line_no   = [int]$line_no
-        exec_time = [float]$exec_time
-        statement = $statement;
+        if(-not $measures.Contains($line_no)) {
+            $entry = [pscustomobject]@{
+                exec_time = $exec_time;
+                line_no   = $line_no;
+                statement = $statement;
+            }
+
+            $measures.Add($line_no, $entry);
+        } else {
+            $measures[$line_no].exec_time += $exec_time;
+        }
+
+        $acc_time += $measures[$line_no].exec_time;
     }
 
-    $entries += $obj;
+    echo $acc_time;
 }
 
-$total_time = 0;
-foreach($entry in $entries) {
-    $total_time += $entry.exec_time;
-    if($null -eq $slowest_entry -or $entry.exec_time -gt $slowest_entry.exec_time) {
-        $slowest_entry = $entry;
-    }
-}
-
-echo $total_time
-echo $slowest_entry
+## Display table...
+$measures.Values | Sort-Object -Property "exec_time" -Descending;
+echo $acc_time;
