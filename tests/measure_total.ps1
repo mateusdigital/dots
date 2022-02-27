8ca313bc67e19794a6fb5ab111398c4ea63b6254
stdmatt <stdmatt@pixelwizards.io>
2022-02-27 14:20:09 -0300

    - [WIP] Add total time and better logging...

--- tests/measure_total.ps1
+++ tests/measure_total.ps1
@@ -3,18 +3,23 @@
 $SCRIPT_DIR          = (sh_dirpath $MyInvocation.InvocationName)
 $MEASURE_OUTPUT_FILE = (sh_join_path $SCRIPT_DIR "result.txt");
 
-$measures   = @{};
-$acc_time   = 0.0;
-$runs_count = 1;
+$measures          = @{};
+$acc_time          = 0.0;
+$runs_count        = 10;
 $script_to_measure = $profile;
 
 for($i = 0; $i -lt $runs_count; $i += 1) {
+    Write-Progress                      `
+        -Activity "Running Measurement"  `
+        -Status "$i of $runs_count completed..." `
+        -PercentComplete ($i / $runs_count);
+
     ## Measure it and just save:
     ##   Line Number - Time Taken - Line
     Measure-Script $PROFILE       `
         | tail +7                 `
         | grep -v "00:00.0000000" `
-        | grep  "00:00."          `
+        | grep  "00:"          `
         | tr -s " "               `
         | cut -d " " -f 3-1000    `
         > $MEASURE_OUTPUT_FILE
@@ -29,6 +34,7 @@ for($i = 0; $i -lt $runs_count; $i += 1) {
 
         if(-not $measures.Contains($line_no)) {
             $entry = [pscustomobject]@{
+                cost      = 0;
                 exec_time = $exec_time;
                 line_no   = $line_no;
                 statement = $statement;
@@ -39,12 +45,14 @@ for($i = 0; $i -lt $runs_count; $i += 1) {
             $measures[$line_no].exec_time += $exec_time;
         }
 
-        $acc_time += $measures[$line_no].exec_time;
+        $acc_time += $exec_time;
     }
+}
 
-    echo $acc_time;
+foreach($item in $measures.Values) {
+    $item.cost = (($item.exec_time / $acc_time) * 100);
 }
 
 ## Display table...
 $measures.Values | Sort-Object -Property "exec_time" -Descending;
-echo $acc_time;
+echo "Total time: $acc_time";
