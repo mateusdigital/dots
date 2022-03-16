52d0b8accd1436e9488c9da20b24bc7bb204fc59
stdmatt <stdmatt@pixelwizards.io>
2022-03-16 19:03:28 -0300

    [REFACTOR] Put the variables in the scope of the function

--- source/main.ps1
+++ source/main.ps1
@@ -9,19 +9,6 @@ $env:SHLIB_IS_VERSBOSE           = 1;
 
 
 ##----------------------------------------------------------------------------##
-## Info                                                                       ##
-##----------------------------------------------------------------------------##
-##------------------------------------------------------------------------------
-$PROGRAM_NAME            = "dots";
-$PROGRAM_VERSION         = "3.0.0";
-$PROGRAM_AUTHOR          = "stdmatt - <stdmatt@pixelwizads.io>";
-$PROGRAM_COPYRIGHT_OWNER = "stdmatt";
-$PROGRAM_COPYRIGHT_YEARS = "2021, 2022";
-$PROGRAM_DATE            = "30 Nov, 2021";
-$PROGRAM_LICENSE         = "GPLv3";
-
-
-##----------------------------------------------------------------------------##
 ## PSReadLine                                                                 ##
 ##----------------------------------------------------------------------------##
 ##------------------------------------------------------------------------------
@@ -102,6 +89,14 @@ function _configure_PATH()
 ##------------------------------------------------------------------------------
 function show_version()
 {
+    $PROGRAM_NAME            = "dots";
+    $PROGRAM_VERSION         = "3.0.0";
+    $PROGRAM_AUTHOR          = "stdmatt - <stdmatt@pixelwizads.io>";
+    $PROGRAM_COPYRIGHT_OWNER = "stdmatt";
+    $PROGRAM_COPYRIGHT_YEARS = "2021, 2022";
+    $PROGRAM_DATE            = "30 Nov, 2021";
+    $PROGRAM_LICENSE         = "GPLv3";
+
     $value = [string]::Format(
 "{0} - {1} - {2}                                      `
 Copyright (c) {3} - {4}                               `
