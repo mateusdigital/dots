5cee3d0125eb88a566662a13c9a2c12eb842012c
stdmatt <stdmatt@pixelwizards.io>
2022-03-16 14:32:18 -0300

    [NEOVIM] Usable...

--- data/vim/lua/commands.lua
+++ data/vim/lua/commands.lua
@@ -1,2 +1,11 @@
 vim.cmd [[autocmd BufWritePre * %s/\s\+$//e ]]
+vim.api.nvim_exec(
+  [[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost plugins.lua PackerCompile PackerSync
+  augroup end
+]],
+  false
+)
 
--- data/vim/lua/plugins.lua
+++ data/vim/lua/plugins.lua
@@ -1,25 +1,35 @@
-
 --------------------------------------------------------------------------------
 -- Bootstrap                                                                  --
 --------------------------------------------------------------------------------
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  packer_bootstrap = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
 end
 
 vim.cmd [[packadd packer.nvim]]
 
+
 --------------------------------------------------------------------------------
 -- Plugins                                                                    --
 --------------------------------------------------------------------------------
-return require('packer').startup(function()
+return require("packer").startup(function()
   -- Packer
-  use 'wbthomason/packer.nvim'
+  use { "wbthomason/packer.nvim" }
   -- Themes
-  use "tomasiser/vim-code-dark"
+  use { "tomasiser/vim-code-dark" }
   -- Telescope
-  use "nvim-telescope/telescope.nvim"
-  use "nvim-lua/plenary.nvim"
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use { "nvim-telescope/telescope.nvim" }
+  use { "nvim-lua/plenary.nvim" }
+  use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
+  use { "nvim-telescope/telescope-file-browser.nvim" }
+  -- NeoTree
+  use { "kyazdani42/nvim-web-devicons" }
+  use { "MunifTanjim/nui.nvim" }
+  use { "nvim-neo-tree/neo-tree.nvim", branch = "v1.x" }
+  -- Barbar
+  use { "romgrk/barbar.nvim" }
+  -- Treesitter
+  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate all" }
+
 end);
