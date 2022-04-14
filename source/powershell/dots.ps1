35a446f7737d21fb3570c803aa97efedb4d3a3b9
stdmatt <stdmatt@pixelwizards.io>
2022-04-14 14:23:42 -0300

    [REFACTOR] Improving the script...

--- source/powershell/dots.ps1
+++ source/powershell/dots.ps1
@@ -1,17 +1,34 @@
 
+##
 ## Preset stuff...
+##
+
 $env:POWERSHELL_TELEMETRY_OPTOUT = 1;
 $env:SHLIB_IS_VERSBOSE           = 0;
 
-
-
+##
 ## Load shlib
+##
+
 $HOME_DIR = if ($HOME -eq "") { "$env:USERPROFILE" } else { $HOME };
 . "${HOME_DIR}/.stdmatt/lib/shlib/shlib.ps1";
 
+
+##
+## ALIAS
+##
+
+$DOTS_DIR   = (sh_get_script_dir);
+$CONFIG_DIR = "${HOME_DIR}/.config";
+$PS_DIR     = "${CONFIG}/powershell";
+$NV_DIR     = "${CONFIG}/nvim";
+
+
+##
 ## Load everything under the dots dir...
-$DOTS = (sh_get_script_dir);
-foreach($item in (Get-ChildItem $DOTS)) {
+##
+
+foreach($item in (Get-ChildItem $DOTS_DIR)) {
     $name = $item.Name;
     if($name -eq "dots.ps1"                          -or `
        $name -eq "Microsoft.PowerShell_profile.ps1"  -or `
@@ -20,6 +37,6 @@ foreach($item in (Get-ChildItem $DOTS)) {
         continue;
     }
 
-    # sh_log_verbose "Loading: $name";
+    sh_log_verbose "Loading: $name";
     . $item.FullName;
 }
