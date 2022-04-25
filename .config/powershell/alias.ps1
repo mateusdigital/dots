0f5e783e4cf972ef73f3c0764b7bca35da407afb
stdmatt <stdmatt@pixelwizards.io>
2022-04-25 14:26:45 -0300

    [NEW] e and g functions

--- .config/powershell/alias.ps1
+++ .config/powershell/alias.ps1
@@ -20,8 +20,29 @@ Set-Alias -Force -Option AllScope -Name nv  -Value $NVIM;
 ##
 
 Set-Alias -Force -Option AllScope -Name "c" _stdmatt_cd; ## Change dir
-Set-Alias -Force -Option AllScope -Name "e" $NVIM;       ## Edit
 Set-Alias -Force -Option AllScope -Name "f" files;       ## Files
 Set-Alias -Force -Option AllScope -Name "l" _stdmatt_ls; ## List
 
+##
+## Single letter functions
+##
 
+##------------------------------------------------------------------------------
+function e()
+{
+    if($args.Count -eq 0) {
+        & $NVIM ".";
+    } else {
+        & $NVIM $args;
+    }
+}
+
+##------------------------------------------------------------------------------
+function g()
+{
+    if($args.Length -eq 0) {
+        git s;
+    } else {
+        git $args;
+    }
+}
