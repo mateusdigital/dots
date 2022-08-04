1be9df89028d9cd5602c5cc1ba2e6c436afd1112
mateus-earth <matt@cosmicpig.digital>
2022-08-03 21:29:24 -0300

    [PATH] Fix the path components under WSL.

--- .config/powershell/path.ps1
+++ .config/powershell/path.ps1
@@ -26,15 +26,19 @@ function _configure_PATH()
             ## @notice(gnu-tools): Add all the gnu tools to the path
             ## so we can use them in mac without prefixing with g.
             ##    find /usr/local/Cellar -iname "*gnubin" | sort
-            "/usr/local/Cellar/coreutils/9.0_1/libexec/gnubin",
-            "/usr/local/Cellar/ed/1.18/libexec/gnubin",
-            "/usr/local/Cellar/findutils/4.9.0/libexec/gnubin",
-            "/usr/local/Cellar/gawk/5.1.1/libexec/gnubin",
-            "/usr/local/Cellar/gnu-sed/4.8/libexec/gnubin",
-            "/usr/local/Cellar/gnu-tar/1.34/libexec/gnubin",
-            "/usr/local/Cellar/grep/3.7/libexec/gnubin",
-            "/usr/local/Cellar/libtool/2.4.6_4/libexec/gnubin",
-            "/usr/local/Cellar/make/4.3/libexec/gnubin",
+
+            ## This code is very wrong... It meant and work someday, but 
+            ## we can be almost sure that those versions don't match anymore...
+            ##      matt - 22-08-03
+            # "/usr/local/Cellar/coreutils/9.0_1/libexec/gnubin",
+            # "/usr/local/Cellar/ed/1.18/libexec/gnubin",
+            # "/usr/local/Cellar/findutils/4.9.0/libexec/gnubin",
+            # "/usr/local/Cellar/gawk/5.1.1/libexec/gnubin",
+            # "/usr/local/Cellar/gnu-sed/4.8/libexec/gnubin",
+            # "/usr/local/Cellar/gnu-tar/1.34/libexec/gnubin",
+            # "/usr/local/Cellar/grep/3.7/libexec/gnubin",
+            # "/usr/local/Cellar/libtool/2.4.6_4/libexec/gnubin",
+            # "/usr/local/Cellar/make/4.3/libexec/gnubin",
             ## Normal stuff...
             "/usr/local/bin", ## @notice(brew): Homebrew put it's stuff here...
             "/usr/bin",
@@ -50,25 +54,29 @@ function _configure_PATH()
         );
     }
     ##--------------------------------------------------------------------------
-    elseif($sh_is_wsl) {
-        sh_log_verbose "Configuring path for WSL";
-    }
-    ##--------------------------------------------------------------------------
     elseif($IsLinux -or $sh_is_wsl) {
         sh_log_verbose "Configuring path for GNU/Linux";
+        $user_name = (sh_get_user_name);
+        
         $paths_to_add += @(
+            ## My stuff...
+            "${HOME}/.bin",
+            "${HOME}/.bin/dots",
+            "${HOME}/.bin/dots/gnu",
+            
+            "${HOME}/.fzf/bin",
             ## Anything first from powershell...
             "/usr/local/microsoft/powershell/7",
             ## Normal stuff...
-            "/home/linuxbrew/.linuxbrew/bin", ## @notice(brew): Homebrew put it's stuff here...
             "/usr/local/bin", 
             "/usr/bin",
             "/usr/sbin",
             "/bin",
             "/sbin",
-            ## My stuff...
-            "${HOME}/.local/bin",
-            "${HOME}/.fzf/bin"
+            ## VSCode...
+            ##   @notice: For some reason VSCode is not found o pwsh inside bash.
+            ##   So I'm adding it's path here... matt - 22-08-03
+            "/mnt/c/Users/${user_name}/AppData/Local/Programs/Microsoft VS Code/bin/"
         );
     }
     elseif($IsWindows) {
@@ -100,6 +108,6 @@ function path-list()
 $env:PATH_DEFAULT = (_get_default_PATH);
 $env:PATH         = (_configure_PATH  );
 
-if($env:DOTS_IS_VERSBOSE) { 
+if($env:DOTS_IS_VERSBOSE -eq 1) { 
     path-list;
 }
