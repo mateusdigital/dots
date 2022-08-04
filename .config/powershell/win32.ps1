7457518cf0167863f27266031f895d1b1a445d85
mateus-earth <matt@cosmicpig.digital>
2022-08-04 20:18:19 -0300

    [POWERSHELL] Install the fonts on Windows.

--- .config/powershell/win32.ps1
+++ .config/powershell/win32.ps1
@@ -1,32 +1,42 @@
-#
+## 
+. "${HOME}/.lib/shlib/shlib.ps1";
+. "${HOME}/.config/powershell/directories.ps1";
+. "${HOME}/.config/powershell/path.ps1";
+
+
+##
+## Functions
+##
+
 function install_fonts()
 {
-    if((sh_is_windows_or_wsl)) { 
+    if(-not $IsWindows) { 
         sh_log_fatal "Not running on Windows OS...";
         return;
     }
 
-    ## Thanks to: Arkady Karasin - https://stackoverflow.com/a/61035940
-    $FONTS       = 0x14;
-    $COPYOPTIONS = (4 + 16);
-    $OBJ_SHELL   = (New-Object -ComObject Shell.Application);
-
-    $obj_folder = $OBJ_SHELL.Namespace($FONTS);
-    
     $username        = (sh_get_user_name);
     $where_the_fonts_are_installed = "C:/Users/${username}/AppData/Local/Microsoft/Windows/Fonts"; 
+   
+    echo "Installing fonts at: ";
+    echo "  $where_the_fonts_are_installed";
 
-    $fonts_folder = ${BIN_FODER}
-    foreach($font_filename in Get-ChildItem -Path $fonts_folder -File) {
-        $dest_path = "${where_the_fonts_are_installed}/${font_filename}";
+    $fonts_folder = "${DOTS_BIN_DIR}/dots/fonts";
+    (sh_push_dir $fonts_folder);
 
-        if(Test-Path -Path $dest_path) {
-            echo "Font ${font} already installed";
-        } else {
-            echo "Installing ${font_filename}";
+    $FONTS_OBJ = (New-Object -ComObject Shell.Application).Namespace(0x14);
+    foreach($item in (Get-ChildItem -Recurse "*.ttf")) {
+        $filename    = $item.Name;
+        $target_path = "${where_the_fonts_are_installed}/${filename}";
 
-            $copy_flag = [String]::Format("{0:x}", $COPYOPTIONS);
-            $obj_folder.CopyHere($font_filename.fullname, $copy_flag);
+        if((sh_file_exists "$target_path")) { 
+            echo "Font: ($filename) is already installed!";
+        } else { 
+            echo "Font: (${filename}) is installing!";
+            $FONTS_OBJ.CopyHere($item.FullName);
+            Copy-Item $item.FullName $target_path;
         }
     }
+
+    sh_pop_dir;
 }
