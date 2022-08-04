4577f9c5556f70488617f663d62853a9749f9c15
mateus-earth <matt@cosmicpig.digital>
2022-08-03 21:30:02 -0300

    [POWERSHELL] Fix the paths.

--- .config/powershell/main.ps1
+++ .config/powershell/main.ps1
@@ -62,15 +62,15 @@ if($force_install -or (-not $has_ps_fzf)) {
 ##
 
 ##------------------------------------------------------------------------------
-. "./path.ps1";            ## Setup our $PATH.
-. "./pwsh_modules.ps1";    ## Load the Powershell modules.
-. "./net.ps1";             ## Network utilities.
-. "./prompt.ps1";          ## Customize our PS1.
-. "./dots_function.ps1"    ## Make dots a function.
-. "./shell_functions.ps1"; ## 
-. "./version.ps1";         ## Show version and copyright.
+. "${SCRIPT_DIR}/path.ps1";            ## Setup our $PATH.
+. "${SCRIPT_DIR}/pwsh_modules.ps1";    ## Load the Powershell modules.
+. "${SCRIPT_DIR}/net.ps1";             ## Network utilities.
+. "${SCRIPT_DIR}/prompt.ps1";          ## Customize our PS1.
+. "${SCRIPT_DIR}/dots_function.ps1"    ## Make dots a function.
+. "${SCRIPT_DIR}/shell_functions.ps1"; ## 
+. "${SCRIPT_DIR}/version.ps1";         ## Show version and copyright.
 
 ## Load OS Dependent things...
 if((sh_is_windows_or_wsl)) { 
-    . "./win32.ps1"; 
+    . "${SCRIPT_DIR}/win32.ps1"; 
 }
