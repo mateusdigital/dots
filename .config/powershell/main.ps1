60f0a5005465c5f7199d54379195c3d7b30704cb
mateus-earth <matt@cosmicpig.digital>
2022-08-03 21:45:06 -0300

    [POWERSHELL] Don't check dependencies at runtime anymore...

--- .config/powershell/main.ps1
+++ .config/powershell/main.ps1
@@ -9,53 +9,11 @@ $SCRIPT_DIR = (Split-Path -Parent $MyInvocation.MyCommand.Path);
 . "${SCRIPT_DIR}/environment.ps1";
 . "${SCRIPT_DIR}/directories.ps1";
 
-
-
-##
-## Ensure dependencies 
-##
- 
-$DOTS_FORCE_INSTALL = $false; ## If we should install dependencies if we met them.
-
-##------------------------------------------------------------------------------
-$has_shlib       = (Test-Path "${SHLIB_DIR}/shlib.ps1");
-$has_ps_readline = (Get-InstalledModule).Name.Contains("PSReadLine");
-$has_ps_fzf      = (Get-InstalledModule).Name.Contains("PSFzf");
-$force_install   = $DOTS_FORCE_INSTALL;
-
-if($force_install -or (-not $has_shlib)) { 
-    if(-not (Test-Path "${TEMP_DIR}/shlib")) {
-        git clone "https://gitlab.com/mateus-earth-libs/pwsh/shlib" "${TEMP_DIR}/shlib";
-    }
-    
-    & "${TEMP_DIR}/shlib/install.ps1";
-}
-
-if($force_install -or (-not $has_ps_readline)) { 
-    echo "Installing PSReadline...";
-    Install-Module PSReadLine -AllowPrerelease -Force;
-}
-
-if($force_install -or (-not $has_ps_fzf)) {
-    echo "Installing PSFzf...";
-
-    if(-not (Test-Path "${HOME}/.fzf")) {
-        git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf";
-    }
-
-    $false | & "${HOME}/.fzf/install" --xdg; ## We don't want to install nothing extra...
-    
-    echo "Installing module...";
-    Install-Module -Name PSFzf;
-}
-
-
 ##
 ## Load shlib :)
 ##
 
 . "${SHLIB_DIR}/shlib.ps1"; ## Load shlib.
-
 
 ##
 ## Load things!!!
