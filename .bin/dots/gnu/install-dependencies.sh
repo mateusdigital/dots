040faad5701dfee2a92c990d6e709587167c0d57
mateus-earth <matt@cosmicpig.digital>
2022-09-27 16:38:35 -0300

    Update the packages lists...

--- .bin/dots/gnu/install-dependencies.sh
+++ .bin/dots/gnu/install-dependencies.sh
@@ -5,10 +5,14 @@
 ## Nothing more than a big list of "required" packages to all machines....
 ##
 
+
+
 sudo apt-get update  -y && \
 sudo apt-get upgrade -y && \
 sudo apt-get install -y    \
+    alacritty              \
     atool                  \
+    cmake                  \
     build-essential        \
     firefox                \
     gdb                    \
@@ -16,5 +20,8 @@ sudo apt-get install -y    \
     libgimp2.0-dev         \
     libgtk-3-dev           \
     libgtkmm-3.0-dev       \
+    libsdl2-*              \
+    net-tools              \
+    shellcheck             \
     whois                  \
 ;
