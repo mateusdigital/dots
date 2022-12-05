85d3368ca949447587ba21e4c4436ff8436d3631
mateus-earth <matt@cosmicpig.digital>
2022-12-05 20:33:42 -0300

    Remove the nvim and powershell stuff.

--- .config/nvim/lua/plugin_config/telescope.lua
+++
@@ -1,8 +0,0 @@
-require("telescope").load_extension("fzf")
-require("telescope").load_extension("file_browser")
-
-local _m = function(lhs, rhs)
-    vim.api.nvim_set_keymap('n', lhs, rhs, { noremap = true, silent = true })
-end
-
-_m("<leader>f", [[<cmd>lua require('telescope.builtin').find_files()<CR>]])
