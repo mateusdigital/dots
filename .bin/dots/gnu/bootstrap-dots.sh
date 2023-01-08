1cac08f52c9a9431896c0b4501fd04266390fc15
mateus-earth <mateus@cosmicpig.digital>
2023-01-08 12:37:11 -0300

    [CHANGE] Set the git config for dots upon installation

--- .bin/dots/gnu/bootstrap-dots.sh
+++ .bin/dots/gnu/bootstrap-dots.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
 echo "==> Installing mateus-earth's dots!";
+
 set -o errexit ## fail script if command fails. add "|| true" to commands that you allow to fail
 set -o nounset ## exit if undeclared variables are used
 #set -o xtrace ## trace that which has been executed. uncomment for debugging
-echo "==> Clonning repo...";
-git clone --bare https://github.com/mateus-earth/dots "${HOME}/.dots-bare" || exit 1;
-git --git-dir="${HOME}/.dots-bare" --work-tree="${HOME}" checkout || exit 1;
-echo "==> Done...";
 
-\ No newline at end of file
+
+##------------------------------------------------------------------------------
+echo "==> Clonning repo...";
+
+function _git() {
+    git --git-dir="${HOME}/.dots-bare" --work-tree="${HOME}" $@;
+}
+
+git clone --bare https://github.com/mateus-earth/dots "${HOME}/.dots-bare";
+_git checkout;
+
+##------------------------------------------------------------------------------
+echo "==> Setting config...";
+_git config --local status.showUntrackedFiles no;                        ## Reduce noise.
+_git config --local core.excludesfile "${HOME}/.config/.dots_gitignore"; ## Custom gitignore.
+
+echo "==> Done...";
