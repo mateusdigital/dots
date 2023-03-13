081c34e3b373304768c07990ce7b510dbe864105
mateus-earth <mateus@cosmicpig.digital>
2023-03-13 18:16:36 -0300

    improve make-file to get -e

--- .bin/dots/gnu/make-file.sh
+++ .bin/dots/gnu/make-file.sh
@@ -1,16 +1,39 @@
 #!/usr/bin/env bash
+set -e;
 
+##------------------------------------------------------------------------------
 function show_usage() {
-    echo -e "Usage:\n   make-file.sh <path/to/the/file.ext>";
+    echo -e "Usage:\n   make-file.sh <-e | --exec> <path/to/the/file.ext>";
     exit 1;
+
 }
 
-test -z "$1" && show_usage;
+##------------------------------------------------------------------------------
+is_exec="";
+filepath="";
 
-filename="$(basename  "$1")";
-foldername="$(dirname "$1")";
+## Parse the command line.
+for arg in "$@"; do
+    if [ "$arg" == "-e" ] || [ "$arg" == "--exec" ]; then
+        is_exec="true";
+    else
+        filepath="$arg";
+    fi;
+done;
 
+
+test -z "${filepath}" && show_usage;
+
+filename="$(basename  "$filepath")";
+foldername="$(dirname "$filepath")";
+
+## Craete the file and the folder if needed.
 mkdir -p "${foldername}";
 touch "${foldername}/${filename}";
+
+## MArk this as executable.
+if [ "$is_exec" ]; then
+    chmod 744 "${foldername}/${filename}";
+fi;
 
 echo "---> File touch(1)ed at: ${foldername}/${filename}";
