ca1fee1382e342885497f480d1a768d7213b4071
mateus-earth <mateus@cosmicpig.digital>
2023-04-13 12:53:33 -0300

    Install google chrome and vscode too

--- .local/bin/dots/gnu/install-dependencies.sh
+++ .local/bin/dots/gnu/install-dependencies.sh
@@ -28,6 +28,7 @@ readonly software_list=(
     "firefox"
     "gawk"
     "gdb"
+    "gpg"
     "gimp"
     "git-gui"
     "git"
@@ -58,11 +59,24 @@ done;
 ##
 
 ## Nodejs
-
 curl -sL https://deb.nodesource.com/setup_18.x -o /var/tmp/nodesource_setup.sh
 sudo bash /var/tmp/nodesource_setup.sh;
 sudo apt-get install -y nodejs
 
+## Google Chrome 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+rm   ./google-chrome-stable_current_amd64.deb
+
+## VSCODE
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
 
 ##
 ## Let things clean...
