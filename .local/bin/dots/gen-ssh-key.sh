529019d0f5b1093c63113b9168004875287db892
mateus-earth <mateus@cosmicpig.digital>
2023-04-24 12:57:15 -0300

    Improve the script

--- .local/bin/dots/gen-ssh-key.sh
+++ .local/bin/dots/gen-ssh-key.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 set -e;
 
-## Generate a new ssh key.
 readonly WHO_AM_I="$(git whoami)";
-readonly FILENAME="${HOME}/.ssh/id_ed25519.pub";
+readonly DIRNAME="${HOME}/.ssh";
+readonly FILENAME="${DIRNAME}/id_ed25519.pub";
 
+
+mkdir -p "${DIRNAME}";
+
+## Generate a new ssh key.
 if [ ! -f "${FILENAME}" ]; then
     echo "==> Creating ssh key for: (${WHO_AM_I})";
 
     ssh-keygen -t ed25519 -C "${WHO_AM_I}";
     ssh-add "${FILENAME}";
 fi;
+
+## Make sure that the keys directory has the least amount access possible...
+chmod 600 -R "${DIRNAME}/"*;
+
 
 if [ -n "$(which xsel)" ]; then  ## gnu
 	cat "${FILENAME}" | xsel --input --clipboard;
