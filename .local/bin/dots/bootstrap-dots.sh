04cc5e3def39aa446722106383bdee0da7973112
mateusdigital <hello@mateus.digital>
2023-08-07 15:20:58 -0300

    Add some checks and the license header.

--- .local/bin/dots/bootstrap-dots.sh
+++ .local/bin/dots/bootstrap-dots.sh
@@ -1,18 +1,58 @@
 #!/usr/bin/env bash
-
+##~---------------------------------------------------------------------------##
+##                               *       +                                    ##
+##                         '                  |                               ##
+##                     ()    .-.,="``"=.    - o -                             ##
+##                           '=/_       \     |                               ##
+##                        *   |  '=._    |                                    ##
+##                             \     `=./`,        '                          ##
+##                          .   '=.__.=' `='      *                           ##
+##                 +                         +                                ##
+##                      O      *        '       .                             ##
+##                                                                            ##
+##  File      : bootstrap-dots.sh                                             ##
+##  Project   : dots                                                          ##
+##  Date      : 2023-04-11                                                    ##
+##  License   : GPLv3                                                         ##
+##  Author    : mateus.digital <hello@mateus.digital>                         ##
+##  Copyright : mateus.digital - 2023                                         ##
+##                                                                            ##
+##  Description :                                                             ##
+##   Install dots on the system.                                              ##
+##     curl -s <url> | bash                                                   ##
+##---------------------------------------------------------------------------~##
 
 echo "==> Installing Mateus Mesquita's dots!";
 
 set -o errexit ## fail script if command fails. add "|| true" to commands that you allow to fail
 set -o nounset ## exit if undeclared variables are used
-#set -o xtrace ## trace that which has been executed. uncomment for debugging
+
+readonly DOTS_DIR="${HOME}/.dots-bare";
+
+##------------------------------------------------------------------------------
+## Check pre conditions.
+if [ -d "$DOTS_DIR" ]; then
+    echo "==> Already has dots downloaded...";
+    exit;
+fi;
+
+if [ -f "${HOME}/.bashrc" ]; then
+    echo ""${HOME}/.bashrc" present - Backup and remove it...";
+    exit;
+fi;
+
+if [ -f "${HOME}/.profile" ]; then
+    echo ""${HOME}/.profile" present - Backup and remove it...";
+    exit;
+fi;
+
 
 
 ##------------------------------------------------------------------------------
 echo "==> Clonning repo...";
 
 function _git() {
-    git --git-dir="${HOME}/.dots-bare" --work-tree="${HOME}" $@;
+    git --git-dir="${DOTS_DIR}" --work-tree="${HOME}" $@;
 }
 
 git clone --bare https://github.com/mateusdigital/dots "${HOME}/.dots-bare";
