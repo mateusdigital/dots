e8adb1b1d6a72012dbb68516a16379df6c0a1f11
mateusdigital <mateus@cosmicpig.digital>
2023-08-07 22:27:49 +0000

    Split the dependencies in different files so we can install just what we needed.

--- .local/bin/dots/gnu/dependencies/core.sh
+++ .local/bin/dots/gnu/dependencies/core.sh
@@ -2,52 +2,18 @@
 
 set -e; ## Stop on errors.
 
+echo "Installing core dependencies...";
 sudo apt-get install -y \
     atool               \
-    automake            \
-    build-essential     \
-    cmake               \
-    coreutils           \
     curl                \
-    diffutils           \
     ed                  \
-    findutils           \
-    gawk                \
-    gdb                 \
-    gpg                 \
-    gimp                \
-    git-gui             \
     git                 \
     grep                \
-    jq                  \
-    lynx                \
-    make                \
     net-tools           \
     openssh-server      \
-    pandoc              \
     peco                \
-    ripgrep             \
-    shellcheck          \
-    sshfs               \
     tree                \
     wget                \
     whois               \
-    youtube-dl          \
     xsel                \
-    ;
-
-
-##
-## Custom Software
-##
-
-## Nodejs
-if [ -z "$(which node)" ]; then 
-    curl -sL https://deb.nodesource.com/setup_18.x -o /var/tmp/nodesource_setup.sh
-    sudo bash /var/tmp/nodesource_setup.sh;
-    sudo apt-get install -y nodejs
-fi;
-
-sudo npm install -g \
-    live-server     \
     ;
